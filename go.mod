d415536a6ec34579f7f6e4b5553b6b43791e2502
Lukasz Raczylo <lukasz@raczylo.com>
2022-05-28 00:37:20 +0100

    Add really dummy objects comparison.

--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/lukaszraczylo/pandati
 go 1.17
 
 require (
-	github.com/imdario/mergo v0.3.12
+	github.com/imdario/mergo v0.3.13
 	github.com/stretchr/testify v1.7.0
+	github.com/wI2L/jsondiff v0.2.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	github.com/tidwall/gjson v1.14.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
---
+++ structs_test.go
@@ -0,0 +1,133 @@
+package pandati
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ComapreStructsReturnDiff(t *testing.T) {
+	type args struct {
+		a interface{}
+		b interface{}
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantDiff map[string]interface{}
+	}{
+		{
+			name: "Identical structs",
+			args: args{
+				a: struct {
+					A int
+				}{
+					A: 1,
+				},
+				b: struct {
+					A int
+				}{
+					A: 1,
+				},
+			},
+			wantDiff: map[string]interface{}{},
+		},
+		{
+			name: "Replaced value",
+			args: args{
+				a: struct {
+					A int
+				}{
+					A: 1,
+				},
+				b: struct {
+					A int
+				}{
+					A: 2,
+				},
+			},
+			wantDiff: map[string]interface{}{"/A": float64(2)},
+		},
+		{
+			name: "Different structs",
+			args: args{
+				a: struct {
+					A int
+				}{
+					A: 1,
+				},
+				b: struct {
+					A int
+					C int
+				}{
+					A: 1,
+					C: 2,
+				},
+			},
+			wantDiff: map[string]interface{}{},
+		},
+		{
+			name: "Different structs pt 2",
+			args: args{
+				a: struct {
+					A int
+				}{
+					A: 1,
+				},
+				b: struct {
+					A int
+					C int
+				}{
+					A: 2,
+					C: 2,
+				},
+			},
+			wantDiff: map[string]interface{}{"/A": float64(2)},
+		},
+		{
+			name: "Different structs - nested ",
+			args: args{
+				a: struct {
+					A int
+					C struct {
+						D int
+						E bool
+					}
+				}{
+					A: 1,
+				},
+				b: struct {
+					A int
+					C struct {
+						D int
+						E bool
+					}
+				}{
+					A: 1,
+					C: struct {
+						D int
+						E bool
+					}{
+						D: 3,
+						E: true,
+					},
+				},
+			},
+			wantDiff: map[string]interface{}{"/C/D": float64(3), "/C/E": true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			diff, zero, err := CompareStructsReplaced(tt.args.a, tt.args.b)
+			assert.NoError(t, err)
+			if !zero {
+				mapStringInterface := map[string]interface{}{}
+				for _, v := range diff {
+					mapStringInterface[v.Key] = v.Value
+				}
+
+				assert.Equalf(t, tt.wantDiff, mapStringInterface, "diff: %v", mapStringInterface)
+			}
+		})
+	}
+}
