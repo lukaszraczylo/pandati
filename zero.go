239d89e5d04107db5ddfae9862a7512e3184a11b
Lukasz Raczylo <lukasz@raczylo.com>
2023-04-18 14:41:19 +0100

    fixup! fixup! fixup! Improve isZero to remove allocations.

--- zero.go
+++ zero.go
@@ -6,28 +6,7 @@ import (
 
 var zeroStructMap = make(map[reflect.Type]interface{})
 
-func IsZero[T any](v T) bool {
-	value := reflect.ValueOf(v)
-	if value.Kind() == reflect.Ptr && value.IsNil() {
-		return true
-	}
-	if value.Kind() == reflect.Ptr && value.Elem().Kind() == reflect.Struct {
-		value = value.Elem()
-	}
-	zeroValue := reflect.Zero(value.Type())
-	if value.Type().Comparable() {
-		return reflect.DeepEqual(value.Interface(), zeroValue.Interface())
-	}
-	switch value.Kind() {
-	case reflect.Slice, reflect.Array:
-		return value.Len() == 0
-	case reflect.Struct:
-		zeroStruct := zeroStructMap[value.Type()]
-		if zeroStruct == nil {
-			zeroStruct = reflect.New(value.Type()).Elem().Interface()
-			zeroStructMap[value.Type()] = zeroStruct
-		}
-		return reflect.DeepEqual(value.Interface(), reflect.ValueOf(zeroStruct).Elem().Interface())
-	}
-	return false
+func IsZero[T comparable](v T) bool {
+	var zero T
+	return zero == v
 }
